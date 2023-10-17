30bcb9d9f0e4c9a7b2381f513c242208a6b63b05
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-10-17 23:44:15 +0530

    Tailwind and props

---
+++ 03_tailwind_props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='bg-green-500'>Tailwind Test</h1>
+    </>
+  )
+}
+
+export default App
