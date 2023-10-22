35b75014bac6a9eb8a17ccb639b4dd3e00b56e27
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-10-22 23:28:41 +0530

    Password Generator

--- 05_password_generator/src/App.jsx
+++ 05_password_generator/src/App.jsx
@@ -9,7 +9,23 @@ function App() {
   const [password,setPassword] = useState("");
 
   const passGenerator = useCallback(()=>{
-    
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numAllowed){
+      str+="1234567890";
+    }
+    if(charAllowed){
+      str+="!@#$%^&*(){}[];?/`~"
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      
+      let passStr = Math.floor(Math.random()*str.length+1)
+
+      pass = str.charAt(passStr);
+      
+    }
+
   },[length,numAllowed,charAllowed,setPassword])
 
   return (
