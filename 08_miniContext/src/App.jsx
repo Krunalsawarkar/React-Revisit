c734528db52bf9cc84b7b2d5ccbdeb5df635ee34
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-10-30 22:47:06 +0530

    mini Context

--- 08_miniContext/src/App.jsx
+++ 08_miniContext/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserContextProvider from "./context/UserContextProvider";
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>Testing</h1>
-    </>
-  )
+    <UserContextProvider>
+      <h1>Testing</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
