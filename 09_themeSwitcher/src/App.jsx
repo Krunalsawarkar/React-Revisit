056936cbf5aa6daca97ba8a9ef22efc7a7c268a9
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-10-31 23:47:09 +0530

    Theme Switcher Project

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
-  <div className="flex flex-wrap min-h-screen items-center">
-    <div className="w-full">
-      <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-        {/* Theme */}
-      </div>
+  const [themeMode, setThemeMode] = useState("light");
 
-      <div className="w-full max-w-sm mx-auto">{/* card */}</div>
-    </div>
-  </div>;
+  const ligthTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, ligthTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn  />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
