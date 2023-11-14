82efd376dd562697bd08125c5ca6816cb441bb3e
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-11-14 21:42:08 +0530

    Redux Toolkit Todo

--- 11_redux_todo/src/App.jsx
+++ 11_redux_todo/src/App.jsx
@@ -1,12 +1,13 @@
-
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-
   return (
     <>
-          
+      <AddTodo />
+      <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
