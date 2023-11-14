22855faff43cbc339b8a0c544fc975c92bf94c0b
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-11-14 23:30:51 +0530

    Redux Toolkit Todo

--- 11_redux_todo/src/components/AddTodo.jsx
+++ 11_redux_todo/src/components/AddTodo.jsx
@@ -3,17 +3,17 @@ import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
 function AddTodo() {
-  const [input, setinput] = useState("");
+  const [input, setInput] = useState("");
   const dispatch = useDispatch();
 
   const addTodoHandler = (e) => {
-    e.preventDefault;
+    e.preventDefault();
     dispatch(addTodo(input));
-    setinput("");
+    setInput("");
   };
 
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12 text-center">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
--- 11_redux_todo/src/components/Todos.jsx
+++ 11_redux_todo/src/components/Todos.jsx
@@ -8,8 +8,7 @@ function Todos() {
 
   return (
     <>
-      <div>Todos</div>
-      <ul className="list-none">
+      <ul className="list-none p-6">
         {todos.map((todo) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
