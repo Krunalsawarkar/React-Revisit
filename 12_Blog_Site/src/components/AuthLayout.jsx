89095a81ff0adce046b364c70c3d6911c8fcda3c
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-11-27 21:31:13 +0530

    Mega Project

--- 12_Blog_Site/src/components/AuthLayout.jsx
+++ 12_Blog_Site/src/components/AuthLayout.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
 
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
 
-export default function Protected({ children }) {
-  return <div>AuthLayout</div>;
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
